b9b591e98e74f38ce9315d80c1d72ff688ef062b
Rafik Bikmaev <rbikmaev@codemastersintl.com>
2024-03-05 08:20:06 +0300

    Lab1 - Cleanup

--- Lab1.DataSourceEncoding/Archiver.cs
+++ Lab1.DataSourceEncoding/Archiver.cs
@@ -13,48 +13,44 @@ public class Archiver {
         var dictionary = new Dictionary<string, ushort>();
         ushort iterator = 0;
 
-        try {
-            using var inputStream = new FileStream(inputFilePath, FileMode.Open, FileAccess.Read);
-            using var outputStream = new FileStream(GetArchiveFilePath(inputStream), FileMode.Create, FileAccess.Write);
+        using var inputStream = new FileStream(inputFilePath, FileMode.Open, FileAccess.Read);
+        using var outputStream = new FileStream(GetEnsuredArchiveFilePath(inputStream), FileMode.Create, FileAccess.Write);
 
-            var buffer = new byte[BlockSize];
-            int bytesRead;
+        var buffer = new byte[BlockSize];
+        int bytesRead;
 
-            var fileBytes = new List<byte>();
-            fileBytes.AddRange(Encoding.ASCII.GetBytes("32A"));
+        var fileBytes = new List<byte>();
+        fileBytes.AddRange(Encoding.ASCII.GetBytes("32A"));
 
-            var outputFlow = new List<byte>();
+        var outputFlow = new List<byte>();
 
-            while ((bytesRead = inputStream.Read(buffer, 0, BlockSize)) > 0) {
-                if (dictionary.Count >= 65535 || bytesRead < BlockSize) {
-                    inputStream.Seek(-bytesRead, SeekOrigin.Current);
+        while ((bytesRead = inputStream.Read(buffer, 0, BlockSize)) > 0) {
+            if (dictionary.Count >= 65535 || bytesRead < BlockSize) {
+                inputStream.Seek(-bytesRead, SeekOrigin.Current);
 
-                    var tailBuffer = new byte[inputStream.Length - inputStream.Position];
-                    inputStream.Read(tailBuffer, 0, tailBuffer.Length);
+                var tailBuffer = new byte[inputStream.Length - inputStream.Position];
+                inputStream.Read(tailBuffer, 0, tailBuffer.Length);
 
-                    WriteTailBytes(outputFlow, tailBuffer);
-                    break;
-                }
-
-                var bufferStr = BitConverter.ToString(buffer);
-                if (!dictionary.TryGetValue(bufferStr, out var blockValue)) {
-                    dictionary.Add(bufferStr, iterator++);
-                    fileBytes.AddRange(buffer);
-                }
-
-                outputFlow.AddRange(dictionary[bufferStr].ToBytes());
+                WriteTailBytes(outputFlow, tailBuffer);
+                break;
             }
 
-            fileBytes.InsertRange(3, ((ushort)dictionary.Count).ToBytes());
-            fileBytes.AddRange(outputFlow);
+            var bufferStr = BitConverter.ToString(buffer);
+            if (!dictionary.TryGetValue(bufferStr, out var blockValue)) {
+                dictionary.Add(bufferStr, iterator++);
+                fileBytes.AddRange(buffer);
+            }
 
-            outputStream.Write(fileBytes.ToArray());
-        } catch {
-            throw;
+            outputFlow.AddRange(dictionary[bufferStr].ToBytes());
         }
+
+        fileBytes.InsertRange(3, ((ushort)dictionary.Count).ToBytes());
+        fileBytes.AddRange(outputFlow);
+
+        outputStream.Write(fileBytes.ToArray());
     }
 
-    private static string GetArchiveFilePath(FileStream stream) {
+    private static string GetEnsuredArchiveFilePath(FileStream stream) {
         var result = Path.Combine("./OutputData", Path.GetFileName(stream.Name) + ".32a");
         if (!Directory.Exists(result)) {
             Directory.CreateDirectory("./OutputData");
@@ -69,39 +65,34 @@ public class Archiver {
     }
 
     public void Unarchive(string inputFilePath) {
-        try {
-            using var inputStream = new FileStream(inputFilePath, FileMode.Open, FileAccess.Read);
-            using var outputStream = new FileStream(GetUnarchiveFilePath(inputStream), FileMode.Create, FileAccess.Write);
+        using var inputStream = new FileStream(inputFilePath, FileMode.Open, FileAccess.Read);
+        using var outputStream = new FileStream(GetEnsuredUnarchiveFilePath(inputStream), FileMode.Create, FileAccess.Write);
 
-            CheckFormat(inputStream);
-            var dictionary = GenerateDictionary(inputStream);
+        CheckFormat(inputStream);
+        var dictionary = GenerateDictionary(inputStream);
 
-            var buffer = new byte[2];
-            int bytesRead;
+        var buffer = new byte[2];
+        int bytesRead;
 
-            var iterator = 0;
+        var iterator = 0;
 
-            while ((bytesRead = inputStream.Read(buffer, 0, 2)) > 0) {
-                var byteValue = BinaryPrimitives.ReadUInt16BigEndian(buffer);
-                if (byteValue == ControlBytes) {
-                    break;
-                }
-
-                iterator++;
-
-                outputStream.Write(ByteUtils.HexStringToByteArray(dictionary[byteValue]));
+        while ((bytesRead = inputStream.Read(buffer, 0, 2)) > 0) {
+            var byteValue = BinaryPrimitives.ReadUInt16BigEndian(buffer);
+            if (byteValue == ControlBytes) {
+                break;
             }
 
-            buffer = new byte[BlockSize];
-            while ((bytesRead = inputStream.Read(buffer, 0, BlockSize)) > 0) {
-                outputStream.Write(buffer);
-            }
-        } catch {
-            throw;
+            iterator++;
+
+            outputStream.Write(ByteUtils.HexStringToByteArray(dictionary[byteValue]));
         }
+
+        var tailBuffer = new byte[inputStream.Length - inputStream.Position];
+        inputStream.Read(tailBuffer, 0, tailBuffer.Length);
+        outputStream.Write(tailBuffer);
     }
 
-    private static string GetUnarchiveFilePath(FileStream stream) {
+    private static string GetEnsuredUnarchiveFilePath(FileStream stream) {
         var result = Path.Combine("./DecodedData", Path.GetFileName(stream.Name).Replace(".32a", ""));
         if (!Directory.Exists(result)) {
             Directory.CreateDirectory("./DecodedData");
